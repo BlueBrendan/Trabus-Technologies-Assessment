fe54be43902d68eaa4ba2e8ad10518a36a73892c
Brendan <brendanchoudfo@gmail.com>
2022-01-19 16:03:31 -0800

    Corrected percentage calculations (x100), modeled map colors by death rate percentiles, improved tooltip responsiveness

--- map/views.py
+++ map/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from state.models import State, Statistic
+import numpy as np
 
 # Create your views here.
 def map_view(request):
     states = State.objects.all()
     data = {}
+    death_rates = []
     for state in states:
         data[state.title.lower()] = state.death_rate
+        death_rates.append(state.death_rate)
+    # Calculate percentiles
+    death_rates = np.array(death_rates)
+    low = np.percentile(death_rates, 20)
+    medium = np.percentile(death_rates, 40)
+    high = np.percentile(death_rates, 60)
+    veryHigh = np.percentile(death_rates, 80)
     context = {
         'state_data': data,
+        'percentiles': [low, medium, high, veryHigh],
     }
     return render(request, 'map/map.html', context)
--- state/cron.py
+++ state/cron.py
@@ -4,14 +4,14 @@ from pytz import timezone
 from state.models import State, Statistic
 
 def daily_stat_update():
-    today = datetime.datetime.today().astimezone(timezone('US/Pacific')).strftime("%Y-%m-%d")
+    today = datetime.datetime.today().astimezone(timezone('US/Pacific'))
     # Read data from GitHub csv file and store in dataframe
     data = pd.read_csv('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv')
     # Take last 60 rows and store as a list
     data = data.values[-60:].tolist()
     remove = []
     for row in data:
-        if str(row[0]) != today:
+        if str(row[0]) != today.strftime("%Y-%m-%d"):
             remove.append(row)
     # Remove all rows from data that are not from today
     data = [row for row in data if row not in remove]
@@ -28,7 +28,7 @@ def daily_stat_update():
     # Recalculate the death rate for each state and save in the database
     states = State.objects.all()
     for state in states:
-        stat = Statistic.objects.get(date=datetime.datetime.today().astimezone(timezone('US/Pacific')), state=state)
-        death_rate = round(stat.deaths/stat.cases, 3)
+        stat = Statistic.objects.get(date=today, state=state)
+        death_rate = round(100 * (stat.deaths/stat.cases), 3)
         state.death_rate = death_rate
         state.save()
